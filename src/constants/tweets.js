98e08dd4d3dd3986bfb3a9cb93667b0fa26c6c83
Melroune <kerkeb@gmail.com>
2017-03-20 02:47:56 +0100

    first commit

---
+++ src/constants/tweets.js
@@ -0,0 +1,30 @@
+const tweets = [{
+  avatar: 'https://pbs.twimg.com/profile_images/438317495896657920/zoiIwICY_400x400.jpeg',
+  username: 'Abdou',
+  fullname: 'Abdou Kerkeb',
+  content: 'bienvenu, sur ma derniere création un petit site façon twiter pour bien apprendre a maitriser React Js',
+  date: '20/03/2017'
+},
+{
+  avatar: 'https://pbs.twimg.com/profile_images/831556351310585856/P632Y3-t_400x400.jpg',
+  username: 'Melroune',
+  fullname: 'Melroune Lufking',
+  content: 'bienvenu, sur ma derniere création un petit site façon twiter pour bien apprendre a maitriser React Js',
+  date: '19/03/2017'
+},
+{
+  avatar: 'https://pbs.twimg.com/profile_images/836893532711448577/atXwEr1R_400x400.jpg',
+  username: 'heart',
+  fullname: 'heartStone',
+  content: 'Êtes-vous prêts à rejoindre Élise Cherchétoile dans sa nouvelle expédition ?',
+  date: '18/03/2017'
+},
+{
+  avatar: 'https://pbs.twimg.com/profile_images/700756022713946112/LFzBFpv4_400x400.jpg',
+  username: 'Hikkary',
+  fullname: 'Hikkary milk',
+  content: 'Jésus (PSL) a recommandé : « Vous n’atteindrez jamais ce que vous aimez sans que vous ne surmontiez avec patience ce que vous détestez. »?',
+  date: '17/03/2017'
+}];
+
+export default tweets;
---
+++ src/containers/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css';
+import { tweets } from '../../constants';
+
+console.log(tweets);
+const App = () => (
+  <h1>TEST</h1>
+);
+
+export default App;
